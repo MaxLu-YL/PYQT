25ffca756d1ac1f849b97ff10c188aa1d23dac56
MaxLu-YL <460597235@qq.com>
2023-07-21 00:12:15 +0800

    新增打开文件，文件夹

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys, os
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QFileDialog, QAction
 from PyQt5 import QtWidgets, QtGui
 from PyQt5 import QtCore
 from Src.Ui.Ui_MainWindow import Ui_MainWindow
+from Src.Ui.Ui_MainWidget import Ui_MainWidget
 
 class MyMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -36,10 +37,53 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         if event.button() == QtCore.Qt.LeftButton:
             print("单击鼠标左键2")
 
+class MyMainWidget(QWidget, Ui_MainWidget):
+    def __init__(self):
+        super(MyMainWidget, self).__init__()
+        self.setupUi(self)
+        self.seleteAlgButton.clicked.connect(self.openAlgDefFile)
+        self.seleteOutZipButton.clicked.connect(self.setOutZipFilePath)
+
+        self.cwd = os.getcwd()
+
+    def onFileChoose(self):
+        filePath, filetype = QFileDialog.getOpenFileName(self, '选择行为自定义算法行为定义文件', 
+                                                         self.cwd, #起始路径
+                                                         'All Files(*)') #文件扩展名过滤器\\
+        print(filePath, filetype)
+        return filePath
+
+    def openAlgDefFile(self):
+        # aFileChoose = QAction('选择一个文件...', self)
+        # aFileChoose.triggered.connect(self.onFileChoose)
+
+        filePath = self.onFileChoose()
+        self.inAlgFilePath.setText(filePath)
+        pass
+
+
+        
+    def onFileChooseDir(self):
+        dir_choose = QFileDialog.getExistingDirectory(self,
+                                               '选择导出文件夹',
+                                               self.cwd)
+        if dir_choose == '':
+            # print('取消选择')
+            return
+
+        return dir_choose
+
+    def setOutZipFilePath(self):
+        outZipFilePath = self.onFileChooseDir()
+        self.outZipFilePath.setText(outZipFilePath)
+        pass
         
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    window = MyMainWindow()
+    # window = MyMainWindow()
+    # window.show()
+
+    window = MyMainWidget()
     window.show()
     sys.exit(app.exec_())
